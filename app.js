5167f24bf533d60da3aa2282d56175b4c85b90b3
ZeroMinus13 <zerominus13@gmail.com>
2023-03-04 10:47:48 +0500

    adding static middleware

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ const cors = require('cors');
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(express.static('dist'));
 let data = require('./phonedata');
 
 morgan.token('body', (req, res) => {
